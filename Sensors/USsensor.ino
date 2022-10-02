4962c697b95f031e9cfedd668677ca1ee258c1e7
Ahmed El Yamani <63077422+ahmedely@users.noreply.github.com>
2022-10-02 16:31:51 -0400

    Update USsensor.ino

--- Sensors/USsensor.ino
+++ Sensors/USsensor.ino
@@ -9,39 +9,34 @@ void setup() {
 
 }
 
-float duration,distance;
+float duration,distance,speed,x;
 
 
 void loop() {
 
 //send the first cycle (pulse) 
   digitalWrite(trigPin, LOW);
-  //high frequency are ideal for low range 
-  delayMicroseconds(1);
+  //10 us TTL pulse
+  delayMicroseconds(10);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(1);
+  delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 //calculate the time for the complete cycle
   duration = pulseIn(echoPin, HIGH);
 //sound speed might change due to temperature and other factors
-float speed=172.43;//  in m/s
+float speed=343;//  in m/s
 // calculate the distance
-  distance=(speed*duration)/10000;
-
-    Serial.print(distance);
-  Serial.println(" cm");
-  Serial.print(duration);
-  Serial.println(" us");
-  delay(500);
+  distance=((speed*duration)/10000)/2;
 
 
-  if(distance<=15){
+
+
+  if(distance>=0 && distance<=15){
   analogWrite(trigPin,0);
-  
+  //delay(1000);  
   digitalWrite(led,HIGH);
   }
   else if(distance>15 && distance<40){
-      float x;
       
       x=(float)7.2857*distance-109.2857;
       analogWrite(trigPin,x);
@@ -51,4 +46,8 @@ float speed=172.43;//  in m/s
   analogWrite(trigPin, 255);
   digitalWrite(led,HIGH);
   }
+
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(1000);
 }
