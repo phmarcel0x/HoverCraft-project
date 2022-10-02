5fea84cbfb554e1537757bdccf86d903e5703a35
Ahmed El Yamani <63077422+ahmedely@users.noreply.github.com>
2022-10-01 20:04:42 -0400

    Update USsensor.ino

--- Sensors/USsensor.ino
+++ Sensors/USsensor.ino
@@ -21,25 +21,3 @@ void loop() {
   Serial.println(" us");
   delay(500);
 } 
-phmarcel0x — Yesterday at 5:25 PM
-#define LED P5
-
-int main(){
-
-
-DDRB|=(1<<LED); // set the pin as output
-// there is no need to set EchoPin as input, as it is input by default.
-// If you wish to do it anyways: DDRD&=~(1<<EchoPin);
-PORTB&=~(1<<LED); // just in case, not really needed as it is the default value.
-
-while (1) {
-
-   PORTB|=(1<<LED); //start of the pulse
-  _delay_us(1000); // delay for the pulse
-   PORTB&=~(1<<LED); // end of the pulse
-
-// here you can do your reading of EchoPin pulse length, sending the reading, etc.
-// from now on your Arduino code will work.
-//...
-
-}}
