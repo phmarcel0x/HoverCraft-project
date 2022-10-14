9fdeb95455d6a8fc2498894ebfefb158f47288d3
Aryan Shirazi <76165234+AryanSh1380@users.noreply.github.com>
2022-10-14 16:52:14 -0400

    LED D# & L + servo motor done

--- Motors/servo.ino
+++ Motors/servo.ino
@@ -1,6 +1,5 @@
 
 
-
 /* Get tilt angles on X and Y, and rotation angle on Z
     Angles are given in degrees
  License: MIT
@@ -13,6 +12,9 @@
  unsigned long timer = 0;
 Servo myservo; 
 #define LED 13
+#define D3 11
+
+float x;
 
  void setup() {
    Serial.begin(9600);
@@ -32,21 +34,39 @@ Servo myservo;
  void loop() {
    mpu.update();
  if ((millis() - timer) > 10) { // print data every 10ms
-     Serial.print("X : ");
-     Serial.print(mpu.getAngleX());
-     Serial.print("\tY : ");
-     Serial.print(mpu.getAngleY());
-     Serial.print("\tZ : ");
-     Serial.println(mpu.getAngleZ());
+    Serial.print("X : ");
+    Serial.print(mpu.getAngleX());
+    Serial.print("\tY : ");
+    Serial.print(mpu.getAngleY());
+    Serial.print("\tZ : ");
+    Serial.print(mpu.getAngleZ());
+    Serial.print("tX acc : ");
+    Serial.print(mpu.getAccX());
+     Serial.print("\tY acc : ");
+     Serial.print(mpu.getAccY());
+     Serial.print("\tZ acc : ");
+     Serial.println(mpu.getAccZ());
      timer = millis();
    }
-
-  if (mpu.getAngleZ() <= 90 || mpu.getAngleZ() >= -90){
-    myservo.write(mpu.getAngleZ());      
-    delay(15);
-  }
-  else{
+  
+ myservo.write(-(mpu.getAngleZ()-90));
+  
+  if(-(mpu.getAngleZ())>=90 || (-mpu.getAngleZ())<=-90){
     digitalWrite(LED, HIGH);
-  }   
+  } 
+  else{digitalWrite(LED, LOW);
+  } 
 
- }
+  if(  abs(mpu.getAccX()) < 0.01) {
+    analogWrite(D3, 255);
+  } else if( abs(mpu.getAccX()) > 1) {
+    analogWrite(D3, 0);
+  } else if(mpu.getAccX()>=-1 && mpu.getAccX()<=-0.01) {
+    x=257.57*mpu.getAccX()+257.57;
+    analogWrite(D3,x);    
+  } else if(mpu.getAccX()>=0.01 && mpu.getAccX()<=1) {
+    x=-257.57*mpu.getAccX()+257.57;
+    analogWrite(D3,x);    
+  }
+  
+}
